961285877901203d5594c070e30524bf5d5ff9e4
andreyaksenov <andrey.aksenov@jetbrains.com>
2019-02-18 21:31:09 +0300

    Add the ability to edit countries

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -6,4 +6,15 @@ class CountriesController < ApplicationController
   def show
     @country = Country.find(params[:id])
   end
+
+  def edit
+    @country = Country.find(params[:id])
+  end
+
+  def update
+    @country = Country.find(params[:id])
+    country_params = params.require(:country).permit(:name, :full_name)
+    @country.update(country_params)
+    redirect_to @country
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root "countries#index"
   get "countries" => "countries#index"
   get "countries/:id" => "countries#show", as: "country"
+  get "countries/:id/edit" => "countries#edit", as: "edit_country"
+  patch "countries/:id" => "countries#update"
 end
