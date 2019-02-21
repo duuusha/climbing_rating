592cb88078e6bd740c07618591833dd24ffc608e
andreyaksenov <andrey.aksenov@jetbrains.com>
2019-02-21 22:09:34 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root "countries#index"
-  get "countries" => "countries#index"
-  get "countries/:id" => "countries#show", as: "country"
-  get "countries/:id/edit" => "countries#edit", as: "edit_country"
-  patch "countries/:id" => "countries#update"
+  # get "countries" => "countries#index"
+  # get "countries/new" => "countries#new", as: "new_country"
+  # get "countries/:id" => "countries#show", as: "country"
+  # get "countries/:id/edit" => "countries#edit", as: "edit_country"
+  # patch "countries/:id" => "countries#update"
+
+  resources :countries
 end
