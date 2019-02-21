2157248718ec24e4c3d3e927bf2a129675fb9515
andreyaksenov <andrey.aksenov@jetbrains.com>
2019-02-21 22:11:03 +0300

    update controller

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,6 +1,8 @@
 class CountriesController < ApplicationController
   def index
-    @countries = Country.all
+    # @countries = Country.all
+    @countries = Country.order("name")
+    # @countries = Country.specific_countries
   end
 
   def show
@@ -13,8 +15,29 @@ class CountriesController < ApplicationController
 
   def update
     @country = Country.find(params[:id])
-    country_params = params.require(:country).permit(:name, :full_name)
     @country.update(country_params)
     redirect_to @country
+  end
+
+  def new
+    @country = Country.new
+  end
+
+  def create
+    @country = Country.new(country_params)
+    @country.save
+    redirect_to @country
+  end
+
+  def destroy
+    @country = Country.find(params[:id])
+    @country.destroy
+    redirect_to countries_url
+  end
+
+  private
+
+  def country_params
+    params.require(:country).permit(:name, :full_name, :image_file_name)
   end
 end
