16808d89e7962112a9b160020e811f0ccd8edff7
andreyaksenov <andrey.aksenov@jetbrains.com>
2019-02-21 22:10:45 +0300

    extract the image method to a helper

--- app/helpers/countries_helper.rb
+++ app/helpers/countries_helper.rb
@@ -2,4 +2,12 @@ module CountriesHelper
   def format_country_name(name)
     name.upcase
   end
+
+  def image_for(country)
+    if country.image_file_name.blank?
+      image_tag 'russia.png'
+    else
+      image_tag country.image_file_name
+    end
+  end
 end
