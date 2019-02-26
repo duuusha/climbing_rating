4e122048ac54c31304d54d923aa08429e64da88c
andreyaksenov <andrey.aksenov@jetbrains.com>
2019-02-26 18:00:27 +0300

    add nested routes

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,2 +1,6 @@
 class StatesController < ApplicationController
+  def index
+    @country = Country.find(params[:country_id])
+    @states = @country.states
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  resources :states
   root "countries#index"
-  # get "countries" => "countries#index"
-  # get "countries/new" => "countries#new", as: "new_country"
-  # get "countries/:id" => "countries#show", as: "country"
-  # get "countries/:id/edit" => "countries#edit", as: "edit_country"
-  # patch "countries/:id" => "countries#update"
-
-  resources :countries
+  resources :countries do
+    resources :states
+  end
 end
