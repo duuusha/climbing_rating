458a96304be6344d7278d6f9bea86aed90d533ed
andreyaksenov <andrey.aksenov@jetbrains.com>
2019-10-14 11:17:24 +0300

    Add routes for gyms

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'climbing_gyms/index'
+  get 'climbing_gyms/show'
+  get 'climbing_gyms/new'
+  get 'climbing_gyms/create'
   root "countries#index"
   resources :countries do
     resources :states do
       resources :cities
     end
   end
+  resources :climbing_gyms
+
 end
