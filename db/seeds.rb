0b7855e1a1f500cb5227bf8cec3e8e3d81f75a5c
andreyaksenov <andrey.aksenov@jetbrains.com>
2019-10-14 11:17:42 +0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+
+country_file_name = File.join(Rails.root, 'public', 'countries.json')
+state_file_name = File.join(Rails.root, 'public', 'states.json')
+city_file_name = File.join(Rails.root, 'public', 'cities.json')
+country_file = File.read(country_file_name)
+state_file = File.read(state_file_name)
+city_file = File.read(city_file_name)
+country_data_hash = JSON.parse(country_file)
+state_data_hash = JSON.parse(state_file)
+city_data_hash = JSON.parse(city_file)
+
+# country_data_hash.each do |element|
+#   element[1].each do |country|
+#     @country = Country.new
+#     @country.id = country['id']
+#     @country.name = country['name']
+#     @country.save!
+#   end
+# end
+
+# state_data_hash.each do |element|
+#   element[1].each do |state|
+#     @state = State.new
+#     @state.id = state['id']
+#     @state.country_id = state['country_id']
+#     @state.name = state['name']
+#     @state.save!
+#   end
+# end
+
+# city_data_hash.each do |element|
+#   element[1].each do |city|
+#     @city = City.new
+#     @city.id = city['id']
+#     @city.state_id = city['state_id']
+#     @city.name = city['name']
+#     if @city.valid?
+#       @city.save
+#     end
+#   end
+# end
