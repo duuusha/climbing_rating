97d50817836820a83675d0c0c3d78be0c82e788a
andreyaksenov <andrey.aksenov@jetbrains.com>
2019-10-14 11:17:35 +0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_114251) do
+ActiveRecord::Schema.define(version: 2019_09_16_151506) do
 
   create_table "cities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
-    t.bigint "country_id"
-    t.bigint "state_id"
+    t.bigint "state_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["country_id"], name: "index_cities_on_country_id"
     t.index ["state_id"], name: "index_cities_on_state_id"
   end
 
@@ -38,7 +36,6 @@ ActiveRecord::Schema.define(version: 2019_03_07_114251) do
     t.index ["country_id"], name: "index_states_on_country_id"
   end
 
-  add_foreign_key "cities", "countries"
   add_foreign_key "cities", "states"
   add_foreign_key "states", "countries"
 end
