95a8e364614d5a2d304ae30043a45efe0ee1c65d
andreyaksenov <andrey.aksenov@jetbrains.com>
2019-10-14 11:14:48 +0300

    Add cities

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,3 @@
 class City < ApplicationRecord
-  belongs_to :country
   belongs_to :state
 end
--- db/migrate/20190916151506_create_cities.rb
+++ db/migrate/20190916151506_create_cities.rb
@@ -2,8 +2,7 @@ class CreateCities < ActiveRecord::Migration[6.0]
   def change
     create_table :cities do |t|
       t.string :name
-      t.references :country, foreign_key: true
-      t.references :state, foreign_key: true
+      t.references :state, null: false, foreign_key: true
 
       t.timestamps
     end
